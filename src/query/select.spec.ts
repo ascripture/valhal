7dfe6372e81307eaadec6a0cd9d931c96485bf94
Philipp Schladitz <philipp.schladitz@gmail.com>
2021-10-14 11:21:30 +0200

    Add initial value for selecting entities

--- src/query/select.spec.ts
+++ src/query/select.spec.ts
@@ -2,7 +2,7 @@ import { EntityStore } from '../entity-store';
 import { select } from './select';
 
 describe('select', () => {
-  it('selects an entity', done => {
+  it('selects an entity', (done) => {
     const store = new EntityStore<{
       id: string;
       value: number;
@@ -23,13 +23,13 @@ describe('select', () => {
       value: 300,
     });
 
-    select('test2', store).subscribe(result => {
+    select('test2', store).subscribe((result) => {
       expect(result?.value).toEqual(200);
       done();
     });
   });
 
-  it('selects an entity and reacts to changes', done => {
+  it('selects an entity and reacts to changes', (done) => {
     const store = new EntityStore<{
       id: string;
       value: number;
@@ -52,7 +52,7 @@ describe('select', () => {
 
     let expectation = 200;
     let lastRun = false;
-    select('test2', store).subscribe(result => {
+    select('test2', store).subscribe((result) => {
       expect(result?.value).toEqual(expectation);
 
       if (lastRun) {
@@ -64,6 +64,44 @@ describe('select', () => {
       lastRun = true;
       expectation = 900;
       store.updateEntity({
+        id: 'test2',
+        value: 900,
+      });
+    }, 200);
+  });
+
+  it('selects an initial value and reacts to changes', (done) => {
+    const store = new EntityStore<{
+      id: string;
+      value: number;
+    }>();
+
+    store.add({
+      id: 'test',
+      value: 100,
+    });
+
+    store.add({
+      id: 'test3',
+      value: 300,
+    });
+
+    let expectation = 400;
+    let lastRun = false;
+    select('test2', store, {
+      initialValue: { id: 'test2', value: 400 },
+    }).subscribe((result) => {
+      expect(result?.value).toEqual(expectation);
+
+      if (lastRun) {
+        done();
+      }
+    });
+
+    setTimeout(() => {
+      lastRun = true;
+      expectation = 900;
+      store.upsert({
         id: 'test2',
         value: 900,
       });
--- src/query/select.ts
+++ src/query/select.ts
@@ -2,9 +2,14 @@ import { distinctUntilChanged, map } from 'rxjs/operators';
 import { ManyStorable } from '../storable';
 import { mapStoreData } from '../util';
 
-export function select<ID, STATE>(id: ID, store: ManyStorable<STATE, ID>) {
+export function select<ID, STATE>(
+  id: ID,
+  store: ManyStorable<STATE, ID>,
+  options?: { initialValue?: STATE }
+) {
   return store.asEntityObservable().pipe(
-    map(store => mapStoreData(id, store)),
+    map((store) => mapStoreData(id, store)),
+    map((state) => state ?? options?.initialValue),
     distinctUntilChanged()
   );
 }
